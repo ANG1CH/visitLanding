bd5ab8f7226a5a6194bb07ddc3a2a2e5273a8d15
Angich <avdonichevanton@gmail.com>
2025-08-07 19:56:52 +0300

    Adding a feedback form to Telegram

--- app/routers/home.py
+++ app/routers/home.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+import httpx
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
+
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -20,3 +22,23 @@ async def home(request: Request):
             "about": about
         }
     )
+
+# --- Настройки Telegram ---
+TELEGRAM_BOT_TOKEN = "8437082656:AAEOsFsz9G1d7TLDe5qwF-9Sa1d7iPtGIEY"
+TELEGRAM_CHAT_ID = "903259421"
+
+async def send_telegram_message(text: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": text}
+    async with httpx.AsyncClient() as client:
+        await client.post(url, data=payload)
+
+@router.post("/contact")
+async def contact(
+    name: str = Form(...),
+    email: str = Form(...),
+    message: str = Form(...)
+):
+    text = f"Новое сообщение с сайта!\nИмя: {name}\nEmail: {email}\nСообщение: {message}"
+    await send_telegram_message(text)
+    return RedirectResponse(url="/", status_code=303)
